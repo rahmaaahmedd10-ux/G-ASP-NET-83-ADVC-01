bbce6122b144dcb769a2052c79ef6f4d4a3a2eac
Rahma Ahmed <rahmaaahmedd10@gmail.com>
2026-08-24 18:43:16 +0300

    Solve Question 1

--- G-ASP-NET-83-ADVC#01/Program.cs
+++ G-ASP-NET-83-ADVC#01/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+            // Q1: What is a generic class? Why use generics?
+
+            // 1. How to use a Generic Class:
+            // When creating a new object, we specify the actual type inside < > instead of the Type Parameter.
+            // We write the actual data type inside < > right after the 'new' keyword.
+
+            // 2. Why use Generics?
+            // - Type Safety: Prevents runtime errors by catching type mismatches at compile-time.
+            // - Performance: Avoids Boxing/Unboxing overhead when dealing with Value Types.
+            // - Code Reusability: Write the logic once and use it with any data type (DRY Principle).
+
+            #endregion
         }
     }
 }
